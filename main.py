17245315c4b510c0156f32d21b5f09d3f0672d21
RamyGarici <ramymanilgarici@gmail.com>
2025-07-24 19:00:05 +0100

    feat: Adding attendance

--- main.py
+++ main.py
@@ -1,26 +1,103 @@
 import face_recognition
 import cv2
 import numpy as np
+import os 
+from datetime import datetime
 
 
-imgElon = face_recognition.load_image_file('assets/elon_musk.jpg')
-imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+path = 'ImagesAttendance'
+images = []
+classNames = []
+myList = os.listdir(path)
+for cl in myList:
+    curImg=  face_recognition.load_image_file(f'{path}/{cl}')
+    print(curImg)
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+    
 
-imgTest = face_recognition.load_image_file('assets/elon_musk_test.jpg')
-imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
-faceLoc = face_recognition.face_locations(imgElon)[0]
-encodeElon = face_recognition.face_encodings(imgElon)[0]
-cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2) # top, right, bottom, left
+def findEncodings(images):
+    encodelist = []
+    for img in images:
+        
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodelist.append(encode)   
+    return encodelist
+
+
+def mark_attendance(name):
+    with open('Attendance.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'n{name},{dtString}')
  
-faceLocTest = face_recognition.face_locations(imgTest)[0]
-encodeTest = face_recognition.face_encodings(imgTest)[0]
-cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
+    
+    
+    
+    
+encodelistKnown = findEncodings(images)
 
-results = face_recognition.compare_faces([encodeElon], encodeTest)
-faceDis = face_recognition.face_distance([encodeElon], encodeTest)
-cv2.putText(imgTest,f'{results} {round(faceDis[0],2)} ',(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,255),3)
+cap = cv2.VideoCapture(0)
 
-cv2.imshow('Elon Musk', imgElon)
-cv2.imshow('Elon Musk Test', imgTest)
-cv2.waitKey(0)
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+ 
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+    
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodelistKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodelistKnown, encodeFace)
+        
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            mark_attendance(name)
+            
+            
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
+           
+
+
+
+
+
+# imgElon = face_recognition.load_image_file('assets/elon_musk.jpg')
+# imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+
+# imgTest = face_recognition.load_image_file('assets/elon_musk_test.jpg')
+# imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+
+# faceLoc = face_recognition.face_locations(imgElon)[0]
+# encodeElon = face_recognition.face_encodings(imgElon)[0]
+# cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2) # top, right, bottom, left
+ 
+# faceLocTest = face_recognition.face_locations(imgTest)[0]
+# encodeTest = face_recognition.face_encodings(imgTest)[0]
+# cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
+
+# results = face_recognition.compare_faces([encodeElon], encodeTest)
+# faceDis = face_recognition.face_distance([encodeElon], encodeTest)
+# cv2.putText(imgTest,f'{results} {round(faceDis[0],2)} ',(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,255),3)
+
+# cv2.imshow('Elon Musk', imgElon)
+# cv2.imshow('Elon Musk Test', imgTest)
+# cv2.waitKey(0)
